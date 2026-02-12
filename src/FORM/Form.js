5aa25a9f42c8a05f55131b3bf4e93c4ad9cb5790
ajain27 <a4ankit27@gmail.com>
2026-02-12 15:02:27 -0800

    Form fields sample

---
+++ src/FORM/Form.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./style.css";
+
+function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: "",
+      email: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmission}>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          placeholder="Enter name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+        <button>Enter</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
